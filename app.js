98c5b46544312c24402b5ee85a9dab94474c74a9
nantachai <bz.nantachai@gmail.com>
2023-09-22 16:33:15 +0700

    add exmaple for todo list

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+
+app.use(cors());
 
 // connect mongodb database
 mongoose.connect(process.env.MONGODB_URI, {
@@ -66,6 +72,70 @@ app.delete("/api/:id", async (req, res) => {
   const itemId = req.params.id;
   try {
     const deletedItem = await Data.findByIdAndRemove(itemId);
+
+    if (!deletedItem) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    res.status(204).json(); // Respond with no content for successful deletion
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// model todo list
+
+const todoListSchema = new mongoose.Schema({
+  name: String,
+  time: Date,
+  success: Boolean,
+});
+
+const TodoList = mongoose.model("TodoLists", todoListSchema);
+
+// route
+
+app.get("/api/todoList", async (req, res) => {
+  try {
+    const items = await TodoList.find();
+    res.status(200).json(items);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post("/api/todoList", async (req, res) => {
+  try {
+    const newItem = new TodoList(req.body);
+    await newItem.save();
+    res.status(201).json(newItem);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.put("/api/todoList/:id", async (req, res) => {
+  const itemId = req.params.id;
+  try {
+    const updatedItem = await TodoList.findByIdAndUpdate(itemId, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedItem) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    res.status(200).json(updatedItem);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.delete("/api/todolist/:id", async (req, res) => {
+  const itemId = req.params.id;
+  try {
+    const deletedItem = await TodoList.findByIdAndRemove(itemId);
 
     if (!deletedItem) {
       return res.status(404).json({ message: "Item not found" });
